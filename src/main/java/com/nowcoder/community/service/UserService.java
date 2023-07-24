64946e2099b85e35d26fa8c3a8a7327bdeb940a4
Muki <760370446@qq.com>
2023-07-24 17:13:08 +0800

    修改头像功能已完成

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -173,4 +173,10 @@ public class UserService implements CommunityConstant {
     public LoginTicket findLoginTiclet (String ticket) {
         return loginTicketMapper.selectByTicket(ticket);
     }
+
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+
+
 }
