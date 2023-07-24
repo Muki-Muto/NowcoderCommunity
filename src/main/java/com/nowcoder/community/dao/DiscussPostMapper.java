1c1fabea867a56fd81bdf82780cc346c2adc5b38
Muki <760370446@qq.com>
2023-07-24 21:37:44 +0800

    添加发布帖子功能

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -23,4 +23,6 @@ public interface DiscussPostMapper {
     // 如果只有一个参数,并且在<if>里使用(就是这个参数不一定有),则必须加别名.
 
     int selectDiscussPostRows(@Param("userId") int userId);
+
+    int insertDiscussPost(DiscussPost discussPost);
 }
